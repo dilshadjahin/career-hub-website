d85d6b72454bd3b1d5c8628a426f96f7bed696ec
dilshadjahin <dilshadjahin@gmail.com>
2024-01-04 15:05:37 +0600

    job details added

--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -1,6 +1,8 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { AiOutlineDollar } from "react-icons/ai";
+import { saveJobApplication } from "../../Utility/localStorage";
 
 
 const JobDetails = () => {
@@ -11,20 +13,34 @@ const JobDetails = () => {
     console.log(job);
 
     const handleApplyJob = () =>{
+        saveJobApplication(id);
         toast('you have applied toast successfully');
     }
 
     return (
         <div>
-            <h2>Job Details of: </h2>
+            <h2 className="text-3xl font-bold text-center my-16">Job Details  </h2>
             <div className="grid gap-4 md:grid-cols-4">
-                <div className="border md:col-span-3">
-                    <h2 className="text-4xl">
-                        Details Comming here
-                    </h2>
+                <div className="border md:col-span-3 my-16">
+                   <div className="">
+                     <p className="text-base text-[#757575]"><h5 className="text-base font-bold text-[#1A1919]">Job Description:</h5>{job.job_description}</p>
+                   </div>
+                   <div className="my-6">
+                     <p className="text-base text-[#757575]"><h5 className="text-base font-bold text-[#1A1919]">Job Responsibility:</h5>{job.job_responsibility}</p>
+                   </div>
+                   <div>
+                     <p className="text-base text-[#757575]"><h5 className="text-base font-bold text-[#1A1919]">Educational Requirements:</h5>{job.educational_requirements}</p>
+                   </div>
+                   <div className="my-6">
+                     <p className="text-base text-[#757575]"><h5 className="text-base font-bold text-[#1A1919]">Experiences:</h5>{job.experiences}</p>
+                   </div>
                 </div>
-                <div className="border">
-                    <h2 className="text-2xl">side things</h2>
+
+
+                <div className="border my-16 text-center bg-[#E8E8E8]">
+                    <h2 className="text-[#1A1919] text-xl font-bold">Job Details</h2>
+                    <hr className=""/>
+                    <h2 className="text-[#757575]"><AiOutlineDollar className="text-2xl mr-2" />Salary:</h2>
                     <button onClick={handleApplyJob} className="btn btn-primary w-full">Apply Now</button>
                     <ToastContainer />
                 </div>
